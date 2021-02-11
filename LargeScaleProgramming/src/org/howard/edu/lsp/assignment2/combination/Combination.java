53d52701009a4866981365da2fdb611dec338b48
manishfsb <mnshbsnt@gmail.com>
2021-02-11 17:42:18 -0500

    Adding comments

--- LargeScaleProgramming/src/org/howard/edu/lsp/assignment2/combination/Combination.java
+++ LargeScaleProgramming/src/org/howard/edu/lsp/assignment2/combination/Combination.java
@@ -3,17 +3,27 @@ package org.howard.edu.lsp.assignment2.combination;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+/**
+ * 
+ * @class Combination 
+ * Given an input list and a target sum, prints output as subsets of index of elements that add up to the target sum
+ *
+ */
+
 public class Combination {
 
 	  public static ArrayList<ArrayList> findCombinations(int [] input, int target){
-	    ArrayList <ArrayList> finalArr = new ArrayList<>();
-	    
+	    //taking an integer array and an integer target as parameters
+		  
+		ArrayList <ArrayList> finalArr = new ArrayList<>();
+	    //return type which is an Arraylist of Arraylists
 
 	    for (int i = 0; i < input.length; i ++){
 	      if (input[i] == target){
 	        ArrayList <Integer> singleValue = new ArrayList<>(Arrays.asList(i));
 	        finalArr.add(singleValue);
 	        continue;
+	        //in case a single number equals the target
 	      }
 	      
 	      int tempSum = 0;
@@ -25,6 +35,8 @@ public class Combination {
 
 	      innerArr.add(i);
 	      tempArr.add(i);
+	      //innerArr handles cases if two numbers add up to target
+	      //tempArr handles cases if more than two numbers add up to target
 
 	      for (int j = i + 1; j < input.length; j ++){
 
@@ -41,6 +53,7 @@ public class Combination {
 
 	            finalArr.add(newArr);
 	            tempArr.remove(tempArr.size() - 1);
+	            //removing the last element of the ArrayList in case there are duplicates of the most recent number
 	         }
 	        }	
 
@@ -50,6 +63,7 @@ public class Combination {
 	          ArrayList <Integer> newArr = new ArrayList<>();
 	          for (int indice = 0; indice < innerArr.size(); indice++) {
 	          	newArr.add(innerArr.get(indice));
+	          	//iterating through innerArr so that removing the last element doesn't affect the list
 	          }
 	          finalArr.add(newArr);
 	          innerArr.remove(innerArr.size() - 1);
@@ -65,5 +79,3 @@ public class Combination {
 
 
 	}
-
-
--- LargeScaleProgramming/src/org/howard/edu/lsp/assignment2/test/Test.java
+++ LargeScaleProgramming/src/org/howard/edu/lsp/assignment2/test/Test.java
@@ -5,23 +5,35 @@ import java.util.Arrays;
 
 import org.howard.edu.lsp.assignment2.combination.Combination;
 
+/**
+ * 
+ *	@class Test
+ *	Class tests the implementation of Combination class
+ */
 public class Test {
 
 	public static void main(String[] args) {
 		
-	
+	/*
+	 * 	Input arrays and target are initialized and then methods printInput and printOutput print their respective problem and solutions
+	 */
+		
+		
+	//with negative values
 	int [] input1 = {2,-1,-1};
     int target1 = 1;
     
     printInput(input1, target1);
     printOutput(input1, target1);
-
-    int [] input2 = {5, 5, 10, 15};
+    
+    //one from the homework problem statement
+    int [] input2 = {5, 5, 15, 10};
     int target2 = 15;
     
     printInput(input2, target2);
     printOutput(input2, target2);
-
+    
+    //list with duplicates and zeros
     int [] input3 = {0, 1, 0, 1};
     int target3 = 1;
     
@@ -49,27 +61,29 @@ public class Test {
     printInput(input6, target6);
     printOutput(input6, target6);
     
-    //empty lists
+    //empty list
     int [] input7 = {};
     int target7 = 0;
     
     printInput(input7, target7);
     printOutput(input7, target7);
-
+    
+    //sorted list
     int [] input8 = {1,2,3,4,5,6};
     int target8 = 6;
     
     printInput(input8, target8);
     printOutput(input8, target8);
     
+    //random unsorted lists
     int [] input9 = {1,6,4,5,2,3};
-    int target9 = 6;
+    int target9 = 3;
     
     printInput(input9, target9);
     printOutput(input9, target9);
     
     int [] input10 = {10,1,2,7,6,1,5};
-    int target10 = 8;
+    int target10 = 7;
     
     printInput(input10, target10);
     printOutput(input10, target10);
@@ -79,18 +93,18 @@ public class Test {
 	
 	public static void printInput(int [] inputArr, int target1) {
 		System.out.println("Input list : " + Arrays.toString(inputArr) + '\n'  + "Target: " + target1 );
-	
+		//printing the input list and the target sum
 		
 	}
 	
 	public static void printOutput(int [] inputArr, int target1) {
 
+		//Initializing an instance of Combination class to test all the input
 		Combination	testCase = new Combination();
 		
 		ArrayList <ArrayList> output = testCase.findCombinations(inputArr, target1);
 	    System.out.println("Output: " + Arrays.toString(output.toArray()) + "\n");
-
-		
+	    //Prints the output based on Combination class
 	}
 
 }
